f6cce61bd15a4df7da5134dd000dd293df6278fb
Luiz Carlos <argentinaluiz@gmail.com>
2025-03-10 07:24:48 -0300

    cap05-04-local-storage-validacao-e-renovacao-da-autenticacao

--- spa-frontend/src/router/index.ts
+++ spa-frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import LogoutView from '../views/LogoutView.vue'
 import ProtectedView from '../views/ProtectedView.vue'
+import { ClientTokenBasedHttp } from '../ClientTokenBasedHttp'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,52 @@ const router = createRouter({
       component: ProtectedView,
     },
   ],
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.name === 'login') {
+    next()
+    return
+  }
+
+  if (to.name === 'logout') {
+    next()
+    return
+  }
+
+  const accessToken = localStorage.getItem('accessToken')
+  const refreshToken = localStorage.getItem('refreshToken')
+
+  if (!accessToken || !refreshToken) {
+    next({ name: 'logout' })
+    return
+  }
+
+  if (ClientTokenBasedHttp.isTokenExpiring(refreshToken)) {
+    //melhor verificar se está expirado de fato
+    next({ name: 'logout' })
+    return
+  }
+
+  if (ClientTokenBasedHttp.isTokenExpiring(accessToken)) {
+    const http = new ClientTokenBasedHttp({
+      baseURL: 'http://localhost:3000',
+      accessToken,
+      refreshToken,
+    })
+
+    try {
+      const tokens = await http.doRefreshToken();
+      window.localStorage.setItem('accessToken', tokens.access_token);
+      window.localStorage.setItem('refreshToken', tokens.refresh_token);
+      next();
+      return;
+    } catch (error) {
+      next({ name: 'logout' })
+      return;
+    }
+  }
+  next();
 })
 
 export default router
--- spa-frontend/src/views/ProtectedView.vue
+++ spa-frontend/src/views/ProtectedView.vue
@@ -1,5 +1,15 @@
 <script lang="ts" setup>
-
+import { ClientTokenBasedHttp } from '@/ClientTokenBasedHttp';
+import { ref } from 'vue';
+  const accessToken = localStorage.getItem('accessToken')!
+  const refreshToken = localStorage.getItem('refreshToken')!
+  const http = new ClientTokenBasedHttp({
+      baseURL: 'http://localhost:3000',
+      accessToken,
+      refreshToken,
+    })
+  const user = ref({});  
+  http.get('/protected').then((data) => user.value = data)
 </script>
 
 <template>
