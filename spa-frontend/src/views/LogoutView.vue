e9a6e65d8d72ae05655fc3bed905a93bf342375a
Luiz Carlos <argentinaluiz@gmail.com>
2025-03-10 07:31:02 -0300

    cap05-05-local-storage-logout

--- spa-frontend/src/views/LogoutView.vue
+++ spa-frontend/src/views/LogoutView.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 import router from '@/router'
 
+localStorage.removeItem('accessToken')
+localStorage.removeItem('refreshToken')
+
+router.push('/login')
 </script>
 
 <template>
