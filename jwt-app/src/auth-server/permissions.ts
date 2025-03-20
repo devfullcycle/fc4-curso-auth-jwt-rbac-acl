03c522535c7a5d5c6fa3f926e70a9f77f35cf1a2
Luiz Carlos <argentinaluiz@gmail.com>
2025-03-20 10:10:46 -0300

    cap07-10-iniciando-mapeamento-com-abac

--- jwt-app/src/auth-server/permissions.ts
+++ jwt-app/src/auth-server/permissions.ts
@@ -30,8 +30,8 @@ const rolePermissions: Record<Roles, DefinePermissions> = {
     can("manage", "all"); // admin can manage everything
   },
   Teacher(user, { can }) {
-    can("get", "Course");
-    can("update", "Course");
+    can("get", "Course", { "teacher.user.id": user.id } as any);
+    can("update", "Course",{ "teacher.user.id": user.id } as any);
   },
   Student(user, { can }) {
     can("get", "Course");
